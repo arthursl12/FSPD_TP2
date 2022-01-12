f20638b38c95f7b0910a434aca337615a3015727
arthursl12 <arthursoutolima@gmail.com>
2022-01-12 07:33:09 -0300

    Testing part2

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -26,3 +26,35 @@ for N in $(seq 1 3); do
     fi
     echo "test$N passed"
 done
+
+
+tmux splitw -v -c $(pwd)/ make run_serv_pares_2 arg=5555
+sleep .5
+make run_cli_pares arg=localhost:5555 < $TEST_DIR/test4.txt > $TEST_DIR/output4.txt
+tmux splitw -v -c $(pwd)/ make run_serv_central arg=6666
+sleep .5
+make run_cli_pares arg=localhost:5555 < $TEST_DIR/test5.txt > $TEST_DIR/output5.txt
+make run_cli_central arg=localhost:6666 < $TEST_DIR/test6.txt > $TEST_DIR/output6.txt
+tmux splitw -v -c $(pwd)/ make run_serv_pares_2 arg=7777
+sleep .5
+make run_cli_pares arg=localhost:7777 < $TEST_DIR/test9.txt > $TEST_DIR/output9.txt
+make run_cli_central arg=localhost:6666 < $TEST_DIR/test7.txt > $TEST_DIR/output7.txt
+make run_cli_pares arg=localhost:5555 < $TEST_DIR/test8.txt > $TEST_DIR/output8.txt
+make run_cli_pares arg=localhost:7777 < $TEST_DIR/test10.txt > $TEST_DIR/output10.txt
+
+tmux splitw -v -c $(pwd)/ make run_serv_central arg=8888
+sleep .5
+make run_cli_pares arg=localhost:5555 < $TEST_DIR/test11.txt > $TEST_DIR/output11.txt
+make run_cli_pares arg=localhost:7777 < $TEST_DIR/test12.txt > $TEST_DIR/output12.txt
+make run_cli_central arg=localhost:8888 < $TEST_DIR/test13.txt > $TEST_DIR/output13.txt
+make run_cli_pares arg=localhost:5555 < $TEST_DIR/test14.txt > $TEST_DIR/output14.txt
+make run_cli_pares arg=localhost:7777 < $TEST_DIR/test15.txt > $TEST_DIR/output15.txt
+
+
+for N in $(seq 4 13); do 
+    if ! diff $TEST_DIR/output$N.txt $TEST_DIR/true$N.txt > /dev/null ; then
+        echo "test$N failed"
+        exit 1
+    fi
+    echo "test$N passed"
+done
